9f2c1b217a2aa428b0bcc40c5f0d5be2c7b1c480
Csekő György <gy.cseko@outlook.com>
2021-09-15 18:43:43 +0200

    test case of list of Advertisement

---
+++ src/test/java/com/example/rabittesttask/AdvertisementControllerRestTemplateIT.java
@@ -0,0 +1,53 @@
+package com.example.rabittesttask;
+
+import com.example.rabittesttask.advertisement.Advertisement;
+import com.example.rabittesttask.advertisement.AdvertisementDTO;
+import com.example.rabittesttask.advertisement.AdvertisementService;
+import com.example.rabittesttask.advertisement.CreateAdvertisementCommand;
+import com.example.rabittesttask.user.CreateUserCommandWithNoAdd;
+import com.example.rabittesttask.user.UserDTO;
+import com.example.rabittesttask.user.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+public class AdvertisementControllerRestTemplateIT {
+
+    @Autowired
+    TestRestTemplate template;
+    @Autowired
+    AdvertisementService advertisementService;
+
+    @Test
+    void listOfTheAdds() {
+
+     UserDTO user1 = template.postForObject("/api/users", new CreateUserCommandWithNoAdd("John Doe"), UserDTO.class);
+     UserDTO user2 = template.postForObject("/api/users", new CreateUserCommandWithNoAdd("Jack Doe"), UserDTO.class);
+
+        CreateAdvertisementCommand advertisementCommand1 = new CreateAdvertisementCommand(user1.getId(), "Almavirág hírdetés");
+        AdvertisementDTO advertisementDTO1 = template.postForObject("/api/users/advertisement", advertisementCommand1, AdvertisementDTO.class);
+
+        CreateAdvertisementCommand advertisementCommand2 = new CreateAdvertisementCommand(user2.getId(), "Almavirág hírdetés");
+        AdvertisementDTO advertisementDTO12 = template.postForObject("/api/users/advertisement", advertisementCommand2, AdvertisementDTO.class);
+
+
+
+        List<UserDTO> list = template.exchange("/api/advertisements", HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<UserDTO>>() {
+                }).getBody();
+
+        assertThat(list)
+                .extracting(UserDTO::getFullName)
+                .containsExactly("John Doe", "Jack Doe");
+    }
+
+}
