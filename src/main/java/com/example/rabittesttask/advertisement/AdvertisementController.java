c60f812d2810be7de60525e25a71d0cdd8cd8ade
Csekő György <gy.cseko@outlook.com>
2021-09-15 18:11:46 +0200

    add attributes

--- src/main/java/com/example/rabittesttask/advertisement/AdvertisementController.java
+++ src/main/java/com/example/rabittesttask/advertisement/AdvertisementController.java
@@ -1,4 +1,16 @@
 package com.example.rabittesttask.advertisement;
 
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/advertisement")
+@RestController
+@AllArgsConstructor
+@Tag(name = "Operations on Advertisement")
 public class AdvertisementController {
+
+    private AdvertisementService advertisementService;
 }
--- src/main/java/com/example/rabittesttask/advertisement/AdvertisementService.java
+++ src/main/java/com/example/rabittesttask/advertisement/AdvertisementService.java
@@ -1,4 +1,8 @@
 package com.example.rabittesttask.advertisement;
 
+import org.modelmapper.ModelMapper;
+
 public class AdvertisementService {
+    private AdvertisementRepository advertisementRepository;
+    private ModelMapper modelMapper;
 }
