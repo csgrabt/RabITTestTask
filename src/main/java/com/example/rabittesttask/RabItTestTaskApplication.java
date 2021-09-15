2fc5758549310f4abf35b206648a68e9cd14eb30
Csekő György <gy.cseko@outlook.com>
2021-09-15 13:08:34 +0200

    First commit

---
+++ src/main/java/com/example/rabittesttask/RabItTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabittesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabItTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabItTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rabittesttask/RabItTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabittesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabItTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
