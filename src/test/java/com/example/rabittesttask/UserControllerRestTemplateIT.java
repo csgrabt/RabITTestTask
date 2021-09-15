b9e684f016684b206c3b5ac01dcae594792a6767
Csekő György <gy.cseko@outlook.com>
2021-09-15 15:01:14 +0200

    Test case of the get the users' method. And remove Unnecessary 'public' modifier.

--- src/test/java/com/example/rabittesttask/UserControllerRestTemplateIT.java
+++ src/test/java/com/example/rabittesttask/UserControllerRestTemplateIT.java
@@ -8,15 +8,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 //@Sql(statements =
-public class UserControllerRestTemplateIT {
+class UserControllerRestTemplateIT {
 
     @Autowired
     TestRestTemplate template;
@@ -31,6 +35,22 @@ public class UserControllerRestTemplateIT {
         assertEquals("John Doe", user.getFullName());
 
 
+    }
+
+    @Test
+    void listOfTheUsers() {
+
+        template.postForObject("/api/elders", new CreateUserCommandWithNoAdd("John Doe"), UserDTO.class);
+        template.postForObject("/api/elders", new CreateUserCommandWithNoAdd("Jack Doe"), UserDTO.class);
+
+
+        List<UserDTO> list = template.exchange("/api/users", HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<UserDTO>>() {
+                }).getBody();
+
+        assertThat(list)
+                .extracting(UserDTO::getFullName)
+                .containsExactly("John Doe", "Jack Doe");
     }
 
 }
