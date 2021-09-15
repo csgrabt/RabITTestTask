d1ca1e6ceac98c7e2df57f31567633572cf37539
Csekő György <gy.cseko@outlook.com>
2021-09-15 14:57:50 +0200

    Some unneeded import was deleted.

--- src/main/java/com/example/rabittesttask/user/UserDTO.java
+++ src/main/java/com/example/rabittesttask/user/UserDTO.java
@@ -1,11 +1,11 @@
 package com.example.rabittesttask.user;
 
-import com.example.rabittesttask.advertisement.Advertisement;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
+
 
 @Data
 @AllArgsConstructor
