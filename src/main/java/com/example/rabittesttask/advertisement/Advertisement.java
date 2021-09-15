7ea7283e2edab0d8be6ba394ac88b53d3a4367fb
Csekő György <gy.cseko@outlook.com>
2021-09-15 14:03:28 +0200

    Create the Advertisement Class

---
+++ src/main/java/com/example/rabittesttask/advertisement/Advertisement.java
@@ -0,0 +1,30 @@
+package com.example.rabittesttask.advertisement;
+
+import com.example.rabittesttask.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "advertisements")
+public class Advertisement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @ManyToMany
+    private List<User> users;
+
+    public Advertisement(String title) {
+        this.title = title;
+        this.users = new ArrayList<>();
+    }
+}
