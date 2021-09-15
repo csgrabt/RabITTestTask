33a1ca74d7ee98728e41bd7ab944d7b0cf5f469d
Csekő György <gy.cseko@outlook.com>
2021-09-15 13:12:43 +0200

    Create the User Class

---
+++ src/main/java/com/example/rabittesttask/user/User.java
@@ -0,0 +1,22 @@
+package com.example.rabittesttask.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fullName;
+
+}
+
